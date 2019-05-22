d7392c37602dceb7d0687b0b70ccf00f3bbb3952
altexo <alejandro.riosyb@gmail.com>
2019-05-22 12:06:40 -0600

    Task/tools

--- Models/WorkshopRecordModel.cs
+++ Models/WorkshopRecordModel.cs
@@ -8,13 +8,6 @@ namespace pbshop_web.Models
         public int workshop_record_sate_id {set;get;}
 
     }
-        public class WorkshopRecordWithClientModel : Client
-        {
-        public int wr_id {set;get;}
-        public int client_id {set;get;}
-        public int workshop_record_sate_id {set;get;}
 
-        public List<Client> clients {set;get;}
-    }
 
 }
--- Models/WorkshopRecordStateModel.cs
+++ Models/WorkshopRecordStateModel.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
+
 namespace pbshop_web.Models
 {
     public class WorkshopRecordStateModel
     {
         public int id {set;get;}
-        public string state {set;get;}
+        public string state {set; get;}
+    }
+        public class WorkshopRecordWithClientModel 
+        {
+        public int wr_id {set;get;}
+        public int client_id {set;get;}
+        public int workshop_record_sate_id {set;get;}
+
+        public List<Client> clients {set;get;}
     }
 }
